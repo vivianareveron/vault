801ebd9bb886660ff140fc63f0c68b82110325b4
vivianareveron <viviana@Alfredos-MacBook-Air.local>
2015-04-07 20:39:58 +0200

    Added create button element

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -57,6 +57,7 @@ function logout (){
     app.route = 'logout';
 } 
  
+ 
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
